3a5647d7ca5e9ae0e3c461ee9e4c0abd9e7d2871
Favio Becker <masullibeckerfa@cardiff.ac.uk>
2025-12-25 23:15:41 +0000

    day 2 a done

--- 2025/02/main.lua
+++ 2025/02/main.lua
@@ -1,5 +1,6 @@
 -- get input
-local input = io.open("exampleInput02.txt", "r")
+-- local input = io.open("exampleInput02.txt", "r")
+local input = io.open("input02.txt", "r")
 local converted = input:read("*all")
 input:close()
 
@@ -7,6 +8,30 @@ input:close()
 local ranges = {}
 for each in converted:gmatch("[^,]+") do
 	table.insert(ranges, each)
+end
+
+-- result to print
+local total = 0
+
+-- compare the halves
+
+function has_pattern(num)
+	-- convert to string first
+	local number_string = tostring(num)
+	if #number_string % 2 ~= 0 then
+		return 0
+	end
+
+	-- split if even
+	local middle = #number_string / 2
+	local first_half = number_string:sub(1, middle)
+	local second_half = number_string:sub(middle + 1)
+
+	if first_half == second_half then
+		return num
+	else
+		return 0
+	end
 end
 
 -- now iterate through each range
@@ -19,6 +44,8 @@ for i, range in ipairs(ranges) do
 	local start_num = tonumber(parts[1])
 	local end_num = tonumber(parts[2])
 	for num = start_num, end_num do
-		print(num)
+		total = total + has_pattern(num)
 	end
 end
+
+print(total)
